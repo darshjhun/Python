53b348e673e17c3d736b8bb969be134a8b020654
darshjhun <darsh.jhun@gmail.com>
2023-06-05 19:28:31 +0530

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,50 @@
+print("Hello user")
+
+print("8-4 = ", 8-4)
+print(8 - 4)
+
+print("Number of seconds in a year = ", 365 * 24 * 60 * 60)
+
+print(500 / 20)
+
+print(5 + 30 * 20)
+
+print((5 + 30) * 20)
+
+print(((5 + 30) * 20) / 10)
+
+print(5 + 30 * 20 / 10)
+
+mango_price = 90
+mango_weight = 7
+total = mango_price * mango_weight
+
+print("Total=", total)
+# print("hello comment")
+apple_price = mango_price
+apple_weight = 10
+
+print("Total Apple cost:", apple_price*apple_weight)
+
+
+number_of_days_in_year = 365
+number_of_hour_in_a_day = 24
+number_of_minutes_in_an_hour = 60
+number_of_seconds_in_a_minute = 60
+
+total_number_of_seconds_in_a_year = number_of_days_in_year*number_of_hour_in_a_day*number_of_minutes_in_an_hour*number_of_seconds_in_a_minute
+print("Total number of seconds in a year=", total_number_of_seconds_in_a_year)
+
+pocket_money_per_day = 200
+daily_expense = 100
+saving_per_day = pocket_money_per_day-daily_expense
+saving_in_a_year = saving_per_day*365
+print("saving per day = ", saving_per_day)
+print("Saving in a year = ", saving_in_a_year)
+
+the_tkt = 1500
+dsc = 100
+print("tkt cost per year", (the_tkt-dsc)*365)
+
+print(1500*6)
+
