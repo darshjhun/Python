d7796286908304ac1c8001a2dedfca57bcae016f
darshjhun <darsh.jhun@gmail.com>
2023-06-06 19:51:06 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,4 +1,8 @@
 first_name = "Darsh"
 last_name = "Jhunjhunwal"
-print("My name is ", first_name, last_name)
-print("Welcome",first_name)
+print("My name is", first_name, last_name)
+print("My", "name", "is", first_name, last_name)
+print('Welcome', first_name)
+
+fred = first_name
+print(fred)
