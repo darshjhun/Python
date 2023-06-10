22db3588331155ddf3da99badba1e79206f45eaa
darshjhun <darsh.jhun@gmail.com>
2023-06-10 10:06:09 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,8 +1,8 @@
-first_name = "Darsh"
+first_name = "Priya"
 last_name = "Jhunjhunwal"
 print("My name is", first_name, last_name)
 print("My", "name", "is", first_name, last_name)
-print('Welcome', first_name)
+print('Welcome', first_name, "Greetings!")
 
 fred = first_name
 print(fred)
@@ -18,3 +18,11 @@ subject_2 = "Math"
 print("I'm learning ", subject_1)
 print("didi is learning ", subject_2)
 
+myscore = 100000000
+print("I scored", myscore, "points")
+print("I scored %s points" % myscore)
+
+subject = "ICT"
+
+# I scored 100000000 points in ICT
+print("I scored", myscore, "points in", subject)
