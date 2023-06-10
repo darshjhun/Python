c3df528e0a4dd870bfc3f040f4fe8b0ba1cf420a
darshjhun <darsh.jhun@gmail.com>
2023-06-10 09:40:18 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -7,5 +7,14 @@ print('Welcome', first_name)
 fred = first_name
 print(fred)
 
-print('Darsh\'s')
+double_quote_e = "\tI'm learning \"Python\"\n"
+print(double_quote_e)
+double_quote = "I'm learning Python/n"
+print(double_quote)
+
+subject_1 = "Python"
+subject_2 = "Math"
+
+print("I'm learning ", subject_1)
+print("didi is learning ", subject_2)
 
