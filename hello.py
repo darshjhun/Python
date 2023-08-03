af33446c6ea45172d28859d98ede6c65a3ab9979
darshjhun <darsh.jhun@gmail.com>
2023-08-03 21:33:46 +0530

    lists

--- hello.py
+++ hello.py
@@ -1,1 +1,28 @@
-print('hello world')
+print('hello world'
+      )
+  
+friend_names=['Akshaj','Jatin','Shree','Aarav','Kabeer'
+              ]
+print(friend_names
+      )
+print(friend_names[0]
+      )
+friend_ages=[7,7,8,7,8
+]
+print("In 15 years , %s will be %syears old"%
+      (friend_names[0
+                    ],friend_ages[0
+                                   ]+15
+                                              )
+                                              )
+friend_names[4
+             ]='Darsh'
+             
+print(friend_names
+      )
+print(friend_ages
+      )
+names_and_ages=[friend_names,friend_ages
+                ]
+print(names_and_ages
+      )
